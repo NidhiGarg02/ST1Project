78414a12772f2658bf6f93337b671c16aa0e5202
NidhiGarg02 <101696443+NidhiGarg02@users.noreply.github.com>
2023-11-29 16:14:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./middleware/db");
+db.connectTo();
+app.use(express.json());
+const route = require("./routes/bookRouter");
+app.use('/',route);
+app.get('/',(req,res)=>{
+    res.send("Hey");
+})
+app.get('/services',(req,res)=>{
+    res.send("Servic page");
+})
+app.listen(3000,()=>{
+    console.log("server started successfully");
+})
